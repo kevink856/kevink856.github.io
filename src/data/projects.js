c608fb1e30c9f5134549bf912f425f2d86e11c8f
Kevin Kim <kevink856@gmail.com>
2023-10-04 12:53:08 -0700

    Create projects.js

---
+++ src/data/projects.js
@@ -0,0 +1,30 @@
+const Projects = [
+    {
+        "title": "CSES WebDev Website",
+        "stack": "ReactJS, Django, PostgreSQL, AWS (S3, RDS, CloudFormation)",
+        "desc": "CSES WebDev's main website, housing all relevant information about past and current projects, and about involvement.",
+        "github": "https://github.com/cses-webdev-main/MainWebsite",
+        "website": "https://cses-webdev-main.github.io/MainWebsite/"
+    },
+    {
+        "title": "EloEdge",
+        "stack": "Python, pandas, R, ExpressJS, MongoDB, AWS (EC2)",
+        "desc": "ELO system for college football teams based on 2014-Current performance, projecting power rankings and win percentages.",
+        "website": "http://eloedge.s3-website-us-west-1.amazonaws.com/"
+    },
+    {
+        "title": "communifyy",
+        "stack": "ReactJS, ExpressJS, MongoDB, AWS (Lambda)",
+        "desc": "Web app that displays user's listening stats on Spotify, along with compared stats to other users.",
+        "github": "https://github.com/kevink856/communifyy",
+        "website": "https://kevink856.github.io/communifyy"
+    },
+    {
+        "title": "Graphical Sort",
+        "stack": "Java, Swing, AWT",
+        "desc": "Desktop applet that displays the functionality of several sorting algorithms with animated simulations.",
+        "github": "https://github.com/kevink856/GraphicalSort"
+    }
+];
+
+export default Projects;
