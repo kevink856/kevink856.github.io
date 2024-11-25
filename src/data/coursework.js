ba18c0783f4aa2cd4cb19ecec5927a4b57ff9e17
Kevin Kim <kevink856@gmail.com>
2024-11-25 12:56:52 -0800

    Update coursework/experience

--- src/data/coursework.js
+++ src/data/coursework.js
@@ -1,5 +1,15 @@
 const Coursework = [
     {
+        'code': '[CSE 190]',
+        'title': 'Wireless Embedded Systems',
+        'grade': 'N/A'
+    },
+    {
+        'code': '[CSE 169]',
+        'title': 'Computer Animation',
+        'grade': 'N/A'
+    },
+    {
         'code': '[CSE 168]',
         'title': 'Advanced Graphics',
         'grade': 'A'
@@ -15,8 +25,13 @@ const Coursework = [
         'grade': 'A+'
     },
     {
+        'code': '[CSE 160]',
+        'title': 'Parallel Computing',
+        'grade': 'N/A'
+    },
+    {
         'code': '[CSE 158R]',
-        'title': 'Recommender Systems/Web Mining',
+        'title': 'Recommender Systems',
         'grade': 'N/A'
     },
     {
@@ -60,23 +75,18 @@ const Coursework = [
         'grade': 'A'
     },
     {
-        'code': '[CSE 101]',
-        'title': 'Advanced Algorithms',
-        'grade': 'B'
-    },
-    {
         'code': '[CSE 105]',
         'title': 'Theory of Computation',
         'grade': 'A-'
     },
     {
-        'code': '[CSE 100]',
-        'title': 'Advanced Data Structures',
-        'grade': 'A+'
+        'code': '[CSE 101]',
+        'title': 'Advanced Algorithms',
+        'grade': 'B'
     },
     {
-        'code': '[CSE 30]',
-        'title': 'Systems Programming',
+        'code': '[CSE 100]',
+        'title': 'Advanced Data Structures',
         'grade': 'A+'
     }
 ];
--- src/data/experience.js
+++ src/data/experience.js
@@ -16,10 +16,7 @@ const ExperienceList = [
         "endDate": "Aug 2024",
         "location": "San Diego, CA",
         "points": [
-            "Created recommendation system using cosine similarity in Django and numpy with 6000+ keyword parameters",
-            "Migrated 10000+ LOC MVT site into React Native frontend and DRF API, cutting resource costs by 50%",
-            "Implemented secure real-time chat rooms with Channels websockets using sanitized messages and rate limiting",
-            "Built CI/CD pipeline with GitHub Actions and exposed each stack to domains with an nginx reverse proxy"
+            "Worked on recommender systems, database migration, refactoring"
         ]
     },
     {
@@ -29,22 +26,27 @@ const ExperienceList = [
         "endDate": "June 2024",
         "location": "San Diego, CA",
         "points": [
-            "Developed a FIPS 140-3 cryptographic API in C++ and OpenSSL that supports encryption, authentication, etc",
-            "Utilized UART and TLS protocols to provide a security interface in C between an STM32 MCU and software",
-            "Ensured 100% test code coverage for all libraries with GoogleTest for units and FuzzTest for memory checks",
-            "Maintained a pen-test environment in Kali with clang-tidy, Valgrind and Binwalk for static and dynamic analysis"
+            "Worked on cryptography, serial security, network security, MCUs, pentesting"
         ]
     },
     {
-        "title": "Lead Software Engineer",
-        "company": "CSES WebDev",
+        "title": "Software Lead",
+        "company": "UCSD CSES",
         "startDate": "Aug 2023",
-        "endDate": "Dec 2023",
+        "endDate": "Feb 2024",
         "location": "La Jolla, CA",
         "points": [
-            "Led a team of 5 developers and designers for CSES WebDev's website using React, Python and PostgreSQL",
-            "Ensured security for AWS S3 and RDS services by configuring VPC subnets to internal dev and prod scopes",
-            "Reduced feature bugs by 95% with over 100 automated unit and integration tests using Github Actions"
+            "Led landing site dev team and Activist open source team"
+        ]
+    },
+    {
+        "title": "Tutor / TA",
+        "company": "UC San Diego",
+        "startDate": "Mar 2023",
+        "endDate": "June 2025",
+        "location": "La Jolla, CA",
+        "points": [
+            "Tutor / TA for multiple departments, including CSE 127 (Computer Security)"
         ]
     },
     {
@@ -54,9 +56,7 @@ const ExperienceList = [
         "endDate": "June 2021",
         "location": "Barrington, RI",
         "points": [
-            "Co-founded an organization with 3 peers to virtually teach programming basics to over 600 students globally",
-            "Tutored over 100 students on their final projects to guide and help with API use, OOD, structure, and syntax",
-            "Co-developed several interactive Java programs by pair programming to provide practical examples of concepts"
+            "Created and taught in organization for coding education"
         ]
     }
 ];
